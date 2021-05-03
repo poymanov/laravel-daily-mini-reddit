54904cbbe39fb262eba5d28edb8fa878200dec5a
Nicolay Poymanov <n.poymanov@gmail.com>
2021-05-03 11:13:06 +0300

    Добавлена кнопка удаления комментариев

--- resources/views/post/_comments.blade.php
+++ resources/views/post/_comments.blade.php
@@ -10,10 +10,21 @@
                 <div class="text-sm mb-2">
                     Author: {{ $comment->user->name }} Created: {{ $comment->created_at->diffForHumans() }}
                 </div>
-                @canany(['update'], $comment)
-                    <div class="text-xs mt-3">
+                @canany(['update', 'delete'], $comment)
+                    <div class="text-xs mt-3 flex">
                         @can('update', $comment)
-                            <a href="{{ route('communities.posts.comments.edit', [$community, $post, $comment]) }}" class="p-1 px-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Edit Comment</a>
+                            <a href="{{ route('communities.posts.comments.edit', [$community, $post, $comment]) }}"
+                               class="p-1 px-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 mr-2">Edit Comment</a>
+                        @endcan
+                        @can('delete', $comment)
+                            <form
+                                action="{{ route('communities.posts.comments.destroy', [$community, $post, $comment]) }}"
+                                method="post">
+                                @csrf
+                                @method('delete')
+                                <button
+                                   class="p-1 px-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500" onclick="return confirm('Are you sure?')">Delete Comment</button>
+                            </form>
                         @endcan
                     </div>
                 @endcan
--- tests/Feature/Post/Comment/DeleteTest.php
+++ tests/Feature/Post/Comment/DeleteTest.php
@@ -124,6 +124,91 @@ class DeleteTest extends TestCase
     }
 
     /**
+     * Кнопка удаления не отображается для гостей
+     */
+    public function testDeleteButtonForGuest()
+    {
+        $community = $this->createCommunity();
+        $post      = $this->createPost(['community_id' => $community->id]);
+
+        $comment = $this->createPostComment(['post_id' => $post->id]);
+        $response = $this->get($this->buildPostShowUrl($community->slug, $post->slug));
+
+        $response->assertDontSee('Delete Comment');
+        $response->assertDontSee($this->buildDeleteUrl($community->slug, $post->slug, $comment->id));
+    }
+
+    /**
+     * Кнопка удаления не отображается для пользователей с неподтвержденным email
+     */
+    public function testDeleteButtonForNotVerifiedUser()
+    {
+        $this->signIn(User::factory()->unverified()->create());
+
+        $community = $this->createCommunity();
+        $post      = $this->createPost(['community_id' => $community->id]);
+
+        $comment = $this->createPostComment(['post_id' => $post->id]);
+        $response = $this->get($this->buildPostShowUrl($community->slug, $post->slug));
+
+        $response->assertDontSee('Delete Comment');
+        $response->assertDontSee($this->buildDeleteUrl($community->slug, $post->slug, $comment->id));
+    }
+
+    /**
+     * Кнопка удаления не отображается для комментария другого пользователя
+     */
+    public function testDeleteButtonForNotOwner()
+    {
+        $this->signIn();
+
+        $community = $this->createCommunity();
+        $post      = $this->createPost(['community_id' => $community->id]);
+
+        $comment = $this->createPostComment(['post_id' => $post->id]);
+        $response = $this->get($this->buildPostShowUrl($community->slug, $post->slug));
+
+        $response->assertDontSee('Edit Comment');
+        $response->assertDontSee($this->buildDeleteUrl($community->slug, $post->slug, $comment->id));
+    }
+
+    /**
+     * Кнопка удаления не отображается для комментария, который был создан более суток назад
+     */
+    public function testDeleteButtonExpired()
+    {
+        $user = $this->createUser();
+        $this->signIn($user);
+
+        $community = $this->createCommunity();
+        $post      = $this->createPost(['community_id' => $community->id]);
+
+        $comment   = $this->createPostComment(['post_id' => $post->id, 'user_id' => $user->id, 'created_at' => now()->subDays(2)]);
+        $response = $this->get($this->buildPostShowUrl($community->slug, $post->slug));
+
+        $response->assertDontSee('Delete Comment');
+        $response->assertDontSee($this->buildDeleteUrl($community->slug, $post->slug, $comment->id));
+    }
+
+    /**
+     * Отображение кнопки удаления
+     */
+    public function testDeleteButtonSuccess()
+    {
+        $user = $this->createUser();
+        $this->signIn($user);
+
+        $community = $this->createCommunity();
+        $post      = $this->createPost(['community_id' => $community->id]);
+
+        $comment = $this->createPostComment(['post_id' => $post->id, 'user_id' => $user->id]);
+        $response = $this->get($this->buildPostShowUrl($community->slug, $post->slug));
+
+        $response->assertSee('Delete Comment');
+        $response->assertSee($this->buildDeleteUrl($community->slug, $post->slug, $comment->id));
+    }
+
+    /**
      * Формирование пути для просмотра публикации
      *
      * @param string $communitySlug
